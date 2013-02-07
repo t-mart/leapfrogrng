a21ee39668498c583cf964b3a532d400dfa1161a
Tim Martin <tim@timmart.in>
2013-02-07 04:49:20 -0500

    start proc_fs file construction

--- Makefile
+++ Makefile
@@ -1,17 +1,15 @@
-#just some factor connection helpers
-#!!!!!!make sure to look at ssh_config_entry!!!!!!
+#Top level Makefile
+#Brings together the salient targets from the project and any other misc stuff.
+
+.PHONY: test load unload tunnel
 
 #establish tunnels from
-#	localhost:5904 -> warp1
-#	localhost:60022 -> warp1
+#	localhost:5904 -> warp1:5904 (VNC)
+#	localhost:60022 -> factor004:22 (SSH)
 #and bring up a shell on factor-3210
 tunnel:
 	ssh -L5904:warp1.cc.gatech.edu:5904 -L60022:factor004.cc.gatech.edu:22 factor-3210
 
-vnc:
-	vncviewer localhost:4
-
-.PHONY: test load unload
 test:
 	make -C test
 
--- src/lfrng.c
+++ src/lfrng.c
@@ -1,28 +1,90 @@
-// Source: http://blog.markloiseau.com/2012/04/hello-world-loadable-kernel-module-tutorial/
-// Defining __KERNEL__ and MODULE allows us to access kernel-level code not usually available to userspace programs.
+// Eric,
+//
+// To test, you might find yourself needing to actually get on the factor. So
+// I put together some steps:
+//   follow instructions in ssh_config_entry
+//   make tunnel (in the root Makefile, not src's)
+//   <your_vnc_viewer> localhost:4 (i use tightvnc from my distro's repos)
+//
+// If you want to get a shell without ssh (easier imo),
+//   (make tunnel if not already done)
+//   ssh <username>@localhost:60022
+//
+// Use make load or make unload respectively. I put targets in both Makefile and
+// src/Makefile. Gotta be root.
+//
+// To see the kernel messages,
+//   sudo tail -f /var/log/messages
+// (or run without the -f flag to just print the last few lines)
+//
+// This pages seem like it might come in handy:
+//   www.tldp.org/LDP/lkmpg/2.6/html/lkmpg.html
+//   newdata.box.sk/raven/lkm.html (search for proc_fs)
+//
+// See you in class.
+//
+// -Tim
+
+// Some snippets taken from:
+// http://blog.markloiseau.com/2012/04/hello-world-loadable-kernel-module-tutorial/
+//
+// Defining __KERNEL__ and MODULE allows us to access kernel-level code not
+// usually available to userspace programs.
 #undef __KERNEL__
 #define __KERNEL__
- 
+
 #undef MODULE
 #define MODULE
- 
-// Linux Kernel/LKM headers: module.h is needed by all modules and kernel.h is needed for KERN_INFO.
+
+// Linux Kernel/LKM headers: module.h is needed by all modules and kernel.h is
+// needed for KERN_INFO.
 #include <linux/module.h>    // included for all kernel modules
 #include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>        // included for __init and __exit macros
- 
-static int __init hello_init(void)
-{
-    printk(KERN_INFO "Hello world!\n");
-    return 0;    // Non-zero return means that the module couldn't be loaded.
-}
- 
-static void __exit hello_cleanup(void)
-{
-    printk(KERN_INFO "Cleaning up module.\n");
-}
- 
-module_init(hello_init);
-module_exit(hello_cleanup);
+#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/proc_fs.h>   // give us access to proc_fs
 
-MODULE_LICENSE("GPL");
+#define PROC_F_NAME "lfrng"
+
+//Documentation macros
+#define LICENSE "GPL"
+#define AUTHORS "Eric Huang <ehuang3@gatech.edu>, Tim Martin <tim.martin@gatech.edu>"
+#define DESC "A Leapfrog RNG for Georgia Tech's CS3210, Spring 2013"
+
+//proc file struct
+static struct proc_dir_entry *proc_f;
+
+static int __init lfrng_init(void)
+{
+  printk(KERN_INFO "Initializing lfrng...");
+  /* create the /proc file */
+  proc_f = create_proc_entry(PROC_F_NAME, 0644, NULL);
+
+  if (proc_f == NULL) {
+    printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", PROC_F_NAME);
+    return -ENOMEM;
+  }
+
+  proc_f->owner = THIS_MODULE;
+  proc_f->mode  = S_IFREG | S_IRUGO;
+  proc_f->uid   = 0;
+  proc_f->gid   = 0;
+  //proc_f->size  = 37;
+
+  printk(KERN_INFO "/proc/%s created\n", PROC_F_NAME);
+  return 0;    // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit lfrng_exit(void)
+{
+  printk(KERN_INFO "Exiting lfrng...");
+  remove_proc_entry(PROC_F_NAME, &proc_root);
+  printk(KERN_INFO "done.");
+}
+
+module_init(lfrng_init);
+module_exit(lfrng_exit);
+
+MODULE_LICENSE(LICENSE);
+MODULE_AUTHOR(AUTHORS);
+MODULE_DESCRIPTION(DESC);
+
